f4e9a4f5e318175e876f0c33a3206188052c13a3
Luis Gustavo <161742@upf.br>
2025-11-20 19:44:31 -0300

    Alterações no Readme (inclusão dos participantes ) , implementação do tempo maximo de espera e implementação do pico maximo da fila

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # trabalho_final_estrutura
+# integrantes: 
+# Luis Gustavo Cavalheiro Rossal matricula: 161742 
+# Edson Werner Civa Matricula: 208110 
--- tb_estrutura.cpp
+++ tb_estrutura.cpp
@@ -4,13 +4,8 @@
 #include <string>
 using namespace std;
 
-
-// falta calcular os tempos, e colocar os cout pra eles 
-// e falta calcular o pico de pacientes, no fim ja tem o cout certinho
-
 // salva todos os dados de um atendimento
-struct atendimento
-{
+struct atendimento {
     string senha;
     char prioridade;
     int hh;
@@ -30,6 +25,7 @@ int main(){
     int qtd_amarelo = 0;
     int qtd_verde = 0;
     int qtd_branco = 0;
+
     int total = 0;
     int total_atendidos = 0;
     int pico = 0;
@@ -39,8 +35,16 @@ int main(){
     int atendidos_verde = 0;
     int atendidos_branco = 0;
 
-    while (getline(cin, line))
-    {
+    int max_espera = 0;
+    
+    cout << "-- Sistema de cadastro de pacientes --"<< endl;
+    cout << "Digite C para cadastro de paciente"<< endl; 
+    cout << "digite A para Realizar um Atendimento" << endl;
+    cout << "Digite D para ver o numero total de pacientes e todos os atendimentos"<< endl;
+    cout << "Digite Q para sair do sistema"<< endl;
+
+    while (getline(cin, line)) {
+
         if (line.empty()) continue;
 
         stringstream ss(line);
@@ -49,76 +53,103 @@ int main(){
 
         atendimento n;
 
-
-        if (cmd == 'C') { // salva o paciente na fila da sua prioridade
+        if (cmd == 'C') { 
             ss >> n.senha >> n.prioridade >> n.hh >> n.mm;
+
             if (n.prioridade == 'V'){
                 fila_vermelha.push(n);
                 qtd_vermelha++;
 
-            }else if(n.prioridade == 'A'){
+            } else if(n.prioridade == 'A'){
                 fila_amarelo.push(n);
                 qtd_amarelo++;
 
-            }else if(n.prioridade == 'D'){
+            } else if(n.prioridade == 'D'){
                 fila_verde.push(n);
                 qtd_verde++;
 
-            }else if(n.prioridade == 'B'){
+            } else if(n.prioridade == 'B'){
                 fila_branco.push(n);
                 qtd_branco++;
 
-            }else{
+            } else {
                 cout << "Erro ao cadastrar paciente, prioridade invalida! \n";
-            }        
-        }else if(cmd == 'A'){ //atendimento do paciente, verifica se tem alguem na na fila de prioridade mais alta, se nao tiver tenta a proxima 
-            string hh, mm;
-            ss >> hh >> mm;
+            }
 
-            if (!fila_vermelha.empty()){
+            // atualiza pico corretamente
+            int atual = fila_vermelha.size() + fila_amarelo.size() + fila_verde.size() + fila_branco.size();
+            if (atual > pico) pico = atual;
+
+        } else if (cmd == 'A') {
+
+            string hh_s, mm_s;
+            ss >> hh_s >> mm_s;
+            int h_at = stoi(hh_s);
+            int m_at = stoi(mm_s);
+
+            bool temPaciente = false;
+            atendimento atual;
+
+            if (!fila_vermelha.empty()) {
+                atual = fila_vermelha.front();
                 fila_vermelha.pop();
-                qtd_vermelha--;
                 atendidos_vermelha++;
+                temPaciente = true;
 
-            }else if(!fila_amarelo.empty()){
+            } else if (!fila_amarelo.empty()) {
+                atual = fila_amarelo.front();
                 fila_amarelo.pop();
-                qtd_amarelo--;
                 atendidos_amarelo++;
+                temPaciente = true;
 
-            }else if(!fila_verde.empty()){
+            } else if (!fila_verde.empty()) {
+                atual = fila_verde.front();
                 fila_verde.pop();
-                qtd_verde--;
                 atendidos_verde++;
+                temPaciente = true;
 
-            }else if(!fila_branco.empty()){
+            } else if (!fila_branco.empty()) {
+                atual = fila_branco.front();
                 fila_branco.pop();
-                qtd_branco--;
                 atendidos_branco++;
+                temPaciente = true;
+            }
 
-            }else{
-                cout << hh << " " << mm << " Sem pacientes aguardando atendimento\n"; 
+            if (!temPaciente) {
+                cout << hh_s << " " << mm_s << " Sem pacientes aguardando atendimento\n"; 
                 continue;
             }
+
             total_atendidos++;
 
-        }else if(cmd == 'D'){ // mostra a situacao atual
-            total = qtd_vermelha+qtd_branco+qtd_verde+qtd_amarelo;
+            // Cálculo da espera
+            int chegada_min = atual.hh * 60 + atual.mm;
+            int atendimento_min = h_at * 60 + m_at;
+            int espera = atendimento_min - chegada_min;
+
+            if (espera > max_espera)
+                max_espera = espera;
+
+        } else if (cmd == 'D') {
+            total = qtd_vermelha + qtd_amarelo + qtd_verde + qtd_branco;
             cout << "V:" << qtd_vermelha << " "
-            << "A:" << qtd_amarelo << " "
-            << "D:" <<  qtd_verde << " "
-            << "B:" <<  qtd_branco << " | "
-            << "Atendidos:" << total_atendidos << endl;  
-        }else if(cmd == 'Q'){// mostra o final 
-            total = qtd_vermelha+qtd_branco+qtd_verde+qtd_amarelo;
+                 << "A:" << qtd_amarelo << " "
+                 << "D:" << qtd_verde << " "
+                 << "B:" << qtd_branco << " "
+                 << "Atendidos:" << total_atendidos << endl;
+
+        } else if (cmd == 'Q') {
+            total = qtd_vermelha + qtd_amarelo + qtd_verde + qtd_branco;
             cout << "--- RELATÓRIO FINAL ---\n"
-                << "Total atendidos: " << total_atendidos << endl
-                << "Por prioridade: V=" << atendidos_vermelha << " A=" << atendidos_amarelo << " D=" << atendidos_verde << " B=" << atendidos_branco << endl
-                << "Pico de lotação" << pico << endl
-                << "Espera Maxima:" << endl;
-        }
-
+                 << "Total atendidos: " << total_atendidos << endl
+                 << "Por prioridade: V=" << atendidos_vermelha 
+                 << " A=" << atendidos_amarelo 
+                 << " D=" << atendidos_verde 
+                 << " B=" << atendidos_branco << endl
+                 << "Pico de lotação: " << pico << endl
+                 << "Espera Maxima: " << max_espera << " min" << endl;
+            break;
+        }       
     }
-    
-
     return 0;
 }
